ff2aad771c5e806e4b9c84ed797d5bcdf727c0d9
dyao-16 <54858120+dyao-16@users.noreply.github.com>
2019-09-13 11:06:54 -0300

    Tutorial text
    教程链接

--- serverlessP.ts
+++ serverlessP.ts
@@ -1,3 +1,25 @@
 import { stat, fstat } from "fs";
 
 console.log("Hellow there! 哈喽呀~ \n This is a Serverless tutorial.")
+
+//Lambda Function.
+
+export const myHalder= async (event:any ={}): Promise<any> => {
+
+    console.log("Hello World");
+    const response = JSON.stringify(event, null, 2);
+    return response;
+
+}
+{
+    // TO DO
+
+
+
+  //  callback(Error error, Object results);
+    
+}
+
+
+
+
